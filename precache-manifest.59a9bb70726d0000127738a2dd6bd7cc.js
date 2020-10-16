cc2a1edddd288f034244477a439687f51b2a53b7
Jerome Twell <jtwell1@gmail.com>
2020-10-16 19:33:51 +0100

    Updates

---
+++ precache-manifest.59a9bb70726d0000127738a2dd6bd7cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412fc5bd389bd17128b69b31fb7e5d5b",
+    "url": "/sinceyougothere/index.html"
+  },
+  {
+    "revision": "beeb672ee145a68606a9",
+    "url": "/sinceyougothere/static/css/main.f33e15cd.chunk.css"
+  },
+  {
+    "revision": "0ef968e7c327f3a7a032",
+    "url": "/sinceyougothere/static/js/2.5b97ff0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sinceyougothere/static/js/2.5b97ff0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beeb672ee145a68606a9",
+    "url": "/sinceyougothere/static/js/main.eb6bafb1.chunk.js"
+  },
+  {
+    "revision": "dd5344105b1c29fa5f5f",
+    "url": "/sinceyougothere/static/js/runtime-main.b2517f61.js"
+  }
+]);
